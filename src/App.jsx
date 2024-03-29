f87ea1ea53ba986fce7019dd9689cf5560826539
AnnaViktorivna <anya.zav94@gmail.com>
2024-03-29 22:04:03 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,85 @@ import ImageGallery from "./components/ImageGallery/ImageGallery";
 import FormSearch from "./components/FormSearch/FormSearch";
 
 function App() {
-  const [images, setImages] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [images, setImages] = useState([]);
+  //const [res, setRes] = useState([]);
+  // const [loading, setLoading] = useState(false);
 
+  // useEffect(() => {
+  //   async function fetchPhotos() {
+  //     try {
+  //       setLoading(true);
+  //       const data = await requestImages();
+  //       setImages(data);
+  //     } catch (error) {
+  //       // Тут будемо обробляти помилку
+  //     } finally {
+  //       // 2. Встановлюємо індикатор в false після запиту
+  //       setLoading(false);
+  //     }
+  //   }
+
+  //   fetchPhotos();
+  // }, []);
+  // const handleSearch = () => {
+  //   console.log("query", query);
+  // };
+
+  const [img, setImg] = useState("");
+  const [res, setRes] = useState([]);
+
+  const fetchRequest = async () => {
+    const data = await fetch(
+      `https://api.unsplash.com/search/photos?page=1&query=${img}&client_id=a7ioE0htOPw-O5srRHJVQMt3ROHyiYRBO5_YsKRY6OQ`
+    );
+    const dataJ = await data.json();
+    const result = dataJ.results;
+    console.log(result);
+    setRes(result);
+  };
   useEffect(() => {
-    async function fetchPhotos() {
-      try {
-        setLoading(true);
-        const data = await requestImages();
-        setImages(data);
-      } catch (error) {
-        // Тут будемо обробляти помилку
-      } finally {
-        // 2. Встановлюємо індикатор в false після запиту
-        setLoading(false);
-      }
-    }
-
-    fetchPhotos();
+    fetchRequest();
   }, []);
-  const handleSearch = (query) => {
-    console.log("query", query);
+
+  const Submit = () => {
+    fetchRequest();
+    setImg("");
   };
 
   return (
     <div>
-      <h1>Photo Search</h1>
+      <input
+        className='col-3 form-control-sm py-1 fs-4 text-capitalize border border-3 border-dark'
+        type='text'
+        placeholder='Search Anything...'
+        value={img}
+        onChange={(e) => setImg(e.target.value)}
+      />
+      <button
+        type='submit'
+        onClick={Submit}
+        className='btn bg-dark text-white fs-3 mx-3'
+      >
+        Search
+      </button>
+      <div className='col-12 d-flex justify-content-evenly flex-wrap'>
+        {res.map((val) => {
+          return (
+            <li key={val.id}>
+              <img
+                className='col-3 img-fluid img-thumbnail'
+                src={val.urls.small}
+                alt='val.alt_description'
+              />
+            </li>
+          );
+        })}
+      </div>
+
+      {/* <h1>Photo Search</h1>
       <FormSearch onSearch={handleSearch} />
       {loading && <Loader />}
-      <ImageGallery images={images} />
+      <ImageGallery images={images} /> */}
     </div>
   );
 }
--- src/components/api.js
+++ src/components/api.js
@@ -6,16 +6,3 @@ export const requestImages = async () => {
   );
   return data;
 };
-
-// import axios from "axios";
-
-// const API_KEY = "a7ioE0htOPw-O5srRHJVQMt3ROHyiYRBO5_YsKRY6OQ";
-
-// axios.defaults.baseURL = "https://api.unsplash.com";
-
-// export const requestImages = async (currentPage, per_page, photo) => {
-//   const { data } = await axios.get(
-//     `search/photos?client_id=${API_KEY}&page=${currentPage}&per_page=${per_page}&query=${photo}`
-//   );
-//   return data;
-// };
